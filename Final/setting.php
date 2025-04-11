d16e7852500033e71d541f70e966f18112080847
pankitgajjarpk <pankit.work@gmail.com>
2025-04-11 11:00:20 +0530

    .

--- Final/setting.php
+++ Final/setting.php
@@ -66,19 +66,17 @@ $site_terms_three = $sitesettengs['site_terms_three'];
 <div class="secondary-nav">
     <div class="breadcrumbs-container" data-page-heading="Analytics">
         <header class="header navbar navbar-expand-sm">
-            <a href="javascript:void(0);" class="btn-toggle sidebarCollapse" data-placement="bottom">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
-            </a>
+            <a href="javascript:void(0);" class="btn-toggle sidebarCollapse">&nbsp;</a>
             <div class="d-flex breadcrumb-content">
                 <div class="page-header">
                     <div class="page-title">
-                    </div>    
-                    <nav class="breadcrumb-style-one" aria-label="breadcrumb">
+                    </div>
+                    <nav class="breadcrumb-style-five" aria-label="breadcrumb">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
                             <li class="breadcrumb-item active" aria-current="page"><?php echo $Page_Title; ?></li>
                         </ol>
-                    </nav>    
+                    </nav>
                 </div>
             </div>
         </header>
@@ -139,13 +137,13 @@ $site_terms_three = $sitesettengs['site_terms_three'];
                         </div>
                     </div>
 
-                    <div class="mt-3">
+                    <div class="row mt-3">
                         <div id="inquiryloading"></div>
                         <div class="form-message"></div>
                         <div class="form-error"></div>
                     </div>
                     
-                    <div class="row g-3">
+                    <div class="row mt-3">
                         <div class="col-md-12">
                             <button type="button" id="submitbtn" class="btn btn-success _effect--ripple waves-effect waves-light me-2">Submit</button>
                             <button type="button" onclick="window.location = 'dashboard.php'" class="btn btn-danger _effect--ripple waves-effect waves-light">Cancel</button>
